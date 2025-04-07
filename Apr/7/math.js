b9211a4be00c468279d6d0cd25e95ef93e3df148
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-04-07 11:49:42 -0600

    Update and rename math to math.js

---
+++ Apr/7/math.js
@@ -0,0 +1,95 @@
+// This is experimental, to test if using JavaScript syntax coloring could be useful to me
+// To Mr Warren, here is a function index so you know what each expression means
+//
+// sq(number)   - This expression means <number> squared
+// cu(number)   - This expression means <number> cubed
+// sqrt(number) - This expression means the square root of <number>
+// curt(number) - This expression means the cubic root of <number>
+// pow(num1, num2) - This expression means <num1> ^ <num2>
+//
+// QUICK NOTE: If you see something like 4 ** 5, this simply means 4 to the power of 5
+// ANOTHER QUICK NOTE: This isn't real JavaScript syntax, i am simply using syntax highlighting for things like comments, contrast, etc
+//
+// This whole JavaScript thing is experimental, to see if it can be helpful for me.
+// Without further ado, here is the actual answers:
+
+1[
+  sq(2), // 2 squared is 4
+  sq(5), // 5 squared is 5
+];
+
+2[
+  cu(3), // 3 cubed is 27
+  cu(5), // 5 cubed is 125
+];
+
+3[
+  2 ** 5, // 2 ^ 5 is 32
+  3 ** 5, // 3 ^ 5 is 243
+];
+
+4[
+  1 ** 4, // 1 ^ 4 is 1
+  7 ** 4, // 7 ^ 4 is 2401
+];
+
+5[
+  3 ** 3, // 3 ^ 3 is 27
+  8 ** 3, // 8 ^ 3 is 512
+];
+
+6[
+  4 ** 2, // 4 ^ 2 is 16
+  9 ** 2, // 9 ^ 2 is 81
+];
+
+7[
+  5 ** 3, // 5 ^ 3 is 125
+  7 ** 3, // 7 ^ 3 is 343
+];
+
+8[
+  3 ** 4, // 3 ^ 4 is 81
+  4 ** 4, // 4 ^ 4 is 256
+];
+
+9[
+  2 ** 4, // 2 ^ 4 is 16
+  9 ** 4, // 9 ^ 4 is 6561
+];
+
+10[
+  5 ** 2, // 5 ^ 2 is 25
+  8 ** 2, // 8 ^ 2 is 64
+];
+
+11[
+  4 ** 3, // 64
+  7 ** 3, // 343
+];
+
+12[
+  3 ** 2, // 9
+  9 ** 2, // 81
+];
+
+13[
+  2 ** 4, // 16
+  7 ** 4, // 2401
+];
+
+14[
+  5 ** 2, // 25
+  8 ** 2, // 64
+];
+
+15[
+  3 ** 3, // 27
+  5 ** 3, // 125
+];
+
+16[
+  4 ** 3, // 64
+  5 ** 3, // 125
+];
+
