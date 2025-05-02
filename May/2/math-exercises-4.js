6d89b274bdea3235c6b337387ed98a251533ca36
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-05-02 09:57:18 -0600

    Create math-exercises-4.js

---
+++ May/2/math-exercises-4.js
@@ -0,0 +1,29 @@
+// 4a) Which arrow diagrams represent functions?
+2 => true
+3 => true
+4 => true
+5 => true
+
+// 4b) Which arrow diagrams represent functions?
+4 => false
+
+// 4c) Which arrow diagrams represent functions?
+2 => true
+-2 => NaN
+3 => true
+-3 => NaN
+
+// 5)
+a => true
+b => false
+c => true
+d => false
+
+// 6)
+a => d(n) = 20(n) + 5
+b => f(x) = -6(x) + 4
+c => c(n) = 5(n)
+d => p(n) = 2(n) - 7
+
+// 7)
+a => d = 3t - 5
