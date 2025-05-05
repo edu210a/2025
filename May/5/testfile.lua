10053b0addbee53083968d3f19ae41028f96db01
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-05-05 09:09:50 -0600

    Update testfile.lua

--- May/5/testfile.lua
+++ May/5/testfile.lua
@@ -11,3 +11,16 @@ local Core = {
     Part.Parent = workspace
   end
 }
+
+function WallTrial(position: Vector3)
+  local px = Core.NumberGenerator:NextInteger(-100, 100)
+  local pz = Core.NumberGenerator:NextInteger(-100, 100)
+  local sv = Core.NumberGenerator:NextInteger(10, 30)
+  local sb = Core.NumberGenerator:NextInteger(0, 1)
+  local sx = (sb == 0) and sv or 1
+  local sz = (sb == 1) and sv or 1
+  local rb = Core.NumberGenerator:NextInteger(0, 1)
+  local rx = (rb == 0) and 180 or 0
+
+  Core.CreateWall(Vector3.new(px, 5, pz), Vector3.new(sx, 10, sz), Vector3.new(rx, 0, 0))
+end
