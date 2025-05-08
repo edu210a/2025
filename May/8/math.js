d27c456ee0cacba2a9f7c3e8d7bff16615069375
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-05-08 09:58:39 -0600

    Update and rename math.txt to math.js

---
+++ May/8/math.js
@@ -0,0 +1,14 @@
+
+// 4a)
+thresholdX = (array<int>) [-2, 2];
+thresholdY = (array<int>) [-4, 4];
+
+// 4b)
+thresholdX = (array<int>) [-3, 3];
+thresholdY = (array<int>) [-2, 2];
+
+// 4c)
+thresholdX = (array<int>) [-3, 3];
+thresholdY = (int) 2;
+
+// 5)
