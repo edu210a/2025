819d868545903dc00ec2b5e43b3df72fac981430
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-05-12 12:53:00 -0600

    Update math.js

--- May/8/math.js
+++ May/8/math.js
@@ -12,3 +12,36 @@ thresholdX = (array<int>) [-3, 3];
 thresholdY = (int) 2;
 
 // 5)
+// Due to how it appears
+
+// 6a)
+true;
+
+// 6b)
+false;
+
+// 7a)
+thresholdX = (array<int>) [1, 4];
+thresholdY = (array<int>) [1, 2];
+
+// 7b)
+thresholdX = (array<int>) [1, 3];
+thresholdY = (array<int>) [2, 4];
+
+// 7c)
+thresholdX = (array<int>) [1, 3];
+thresholdY = (array<int>) [1, 4];
+
+// 7d)
+thresholdX = (array<int>) [0, 5];
+thresholdY = (array<int>) [2, 2];
+
+// 8)
+a = false;
+b = false;
+c = true;
+d = false;
+e = false;
+
+// 9a)
+thresholdX = (array<int>)
