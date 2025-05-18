4a51c3f2b57fb85f710ddf192ad6fe1c3fa0a0ea
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-05-18 17:05:31 -0600

    Update math-1.6.js

--- May/18/math-1.6.js
+++ May/18/math-1.6.js
@@ -1,23 +1,207 @@
-// 4a) ========================================
-point1 = {
-  0.0, 
-  7.0
-};
-point2 = {
-  19.0, 
-  11.0
-};
+// WOW! LOTS OF SHITTY MATH! KILL ME KILLEME ME IEMFMEIFMOIMWIOFMWOMIMOEMFWEIMFODSM
+// Let me know if the code format is too complex for your mortal mind
+
+// 4a)
 slope = 0.21052631578;
 
-// 4b) ========================================
-point1 = {
-  0.0,
-  0.0
-};
-point2 = {
-  21.0, 
-  6.5
-};
+// 4b)
 slope = 0.30952380952;
 
-// 5a) ========================================
+// 5a)
+slope = -0.21666666666 (negative)
+
+// 5b)
+pos1 = (-3, -2.7)
+pos2 = (3, -0.7)
+slope = 0.33613445378 (positive)
+
+// 5c)
+pos1 = (1.5, 3)
+pos2 = (1.5 -3)
+NOOOOOOOOO
+
+// 5d)
+positive
+
+// 6a
+a = (-2, 2)
+b = (3, 4)
+rise = 4 - 2 = 2
+run = 3 - (-2) = 5
+slope = 2 / 5 = 0.4
+
+// 6b)
+e = (-4, 3)
+f = (4, 1)
+rise = 1 - 3 = -2
+run = 4 - (-4) = 8
+slope = (-2 / 8) = -1 / 4 = 0.25
+
+// 6c)
+m = (-2, -1)
+k = (3, 1)
+rise = 1 - (-1) = 2
+run = 3 - (-2) = 5
+slope = 2 / 5 = 0.4
+
+// 6d)
+r = (3, 4)
+s = (4, -2)
+rise = -2 - 4 = -6
+run = 4 - 3 = 1
+slope = (-6 / 1) = -6
+
+// 7a)
+changeY = 3
+changeX = 1
+slope = m = (3 / 1) = 3
+
+// 7b)
+changeY = -7
+changeX = 2
+slope = m = (-7 / 2) = -(7 / 2)
+
+// 7c)
+changeY = -2
+changeX = -4
+slope = m = (-2 / -4) = (1 / 2)
+
+// 7d)
+changeY = 1
+changeX = -2
+slope = m = (1 / -2) = -(1 / 2)
+
+// 8a)
+null
+
+// 8b)
+null
+
+// 8c)
+null
+
+// 8d)
+null
+
+// 9)
+null
+
+// 10)
+null
+
+// 11a)
+d = (-2, -4)
+e = (2, 0)
+slope = (0 - (-4)) / (2 - (-2)) = 4 / 4 = 1
+
+// 11b)
+f = (0, -2)
+g = (1, -1)
+slope = (-1 - (-2)) / (1 - 0) = 1 / 1 = 1
+
+// 11c)
+a = 1
+b = 1
+
+// 12)
+null
+
+// 13a)
+i = slope = (6 - 2) / (3 - 1) = 4 / 2 = 2
+ii = slope = (5 - 1) / (8 - 0) = 4 / 8 = 1 / 2
+iii = slope = (-8 - 4) / (3 - (-1)) = -12 / 4 = -3
+iv = slope = (-5 - (-7)) / (-6 - (-12)) = 2 / 6 = 1 / 3
+
+// 14)
+null
+
+
+// 15a)
+rise = 6
+run = 90
+slope = 6 / 90 = 1 / 15 ~ 0.0667
+
+// 15b)
+max = 0.15
+run = 90
+slope = (rise / run) => rise = slope * run
+rise = 0.15 * 90 = 13.5
+
+// 16a)
+drop = 6 * (1 / 4) = 25 / 4
+run = 4feet = 48inches
+slope = ((25 / 4) / 48) = 25 / 192 ~ 0.1302
+
+// 16b)
+drop = 6 * (1 / 4) = 25 / 4
+run = 4feet = 48 inches
+horizontal = drop /  slope
+
+// 16c)
+drop = slope * horizontal
+drop = 0.1302 * 216 ~ 28.17
+
+// 17a)
+line1 = 2
+line2 = -2
+line3 = (1 / 2)
+line4 = -(1 / 2)
+
+// 18a)
+1 slope = (y2 - y1) / (x2 - x1) = (0 - 3) / (5 - 0) = (-3 / 5)
+2 slope = (0 - (-3)) / (5 - 0) = 3 / 5
+3 slope = ((0 - (-3)) / (5 - 0)) = 3 / 5
+4 slope = ((0 - 3) / (-5 - 0)) = (-3 / -5) = 3 / 5
+
+// 18b)
+null
+
+// 19a)
+slope = (0 / run) = 0
+
+// 19b)
+slope = (rise / 0) = NaN
+
+// 20a)
+slope = ((y2 - y1) / (x2 - x1))
+b(6, 2) => (x1, y1)
+c(-3, -5) => (x2, y2)
+slope = ((-5 - 2) / (-3 - 6)) = (-7 / -9) = 7 / 9
+
+// 21a) 
+null
+
+// 22a)
+1 / 12
+
+// 22b)
+condition = slope = (rise / run) < (1 / 12)
+let h = 70;
+cond = (h / run) < (1 / 12)
+70 * 12 < run
+840 < run
+
+// 23)
+null
+
+// 24a)
+1 slope = positive
+2 slope = negative
+3 slope = positive
+4 slope = positive
+5 slope = undefined
+
+// 24b)
+null
+
+// 25a)
+slope = ((583.2 - 172.8) / (216 - 64)) = (410.4 / 152) ~ 2.7
+
+// 25b)
+null
+
+// 25c)
+1 mass = 2.7 * 50
+2 mass = 2.7 * 100
+3 mass = 2.7 * 450
+
