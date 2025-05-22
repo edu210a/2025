c7f74a704d3cc28afc792cede792a4d8170b8eb2
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-05-22 09:48:21 -0600

    Update math-6.2.js

--- May/21/math-6.2.js
+++ May/21/math-6.2.js
@@ -1,4 +1,6 @@
-// 4-9, 12, 13
+// ================
+// #4-6, 8, 9
+// ================
 
 // 4)
 a) 6 / 7
@@ -13,3 +15,57 @@ c) neither
 d) neither
 
 // 6)
+parallel a) 5
+parallel b) 4
+perpendicular a) -(1/5)
+perpendicular b) -(1/4)
+
+// 8a)
+Line1 "AB" = [
+  Vec2(-5, -2),
+  Vec2(1, 5)
+]
+
+Line2 "CD" = [
+  Vec2(-1, -4),
+  Vec2(4, 1)
+]
+
+// 8b)
+Line1 "EF" = [
+  Vec2(-3, 4),
+  Vec2(3, 2)
+]
+
+Line2 "GH" [
+  Vec2(2, 5),
+  Vec2(0, -1)
+]
+
+// 8c)
+Line1 "JK" [
+  Vec2(-2, 3),
+  Vec2(1, -3)
+]
+
+Line2 "MN" [
+  Vec2(3, 1),
+  Vec2(-4, -2)
+]
+
+// 8d)
+Line1 "PQ" [
+  Vec2(0, 5),
+  Vec2(6, 2)
+]
+
+Line2 "RS" [
+  Vec2(-4, -1),
+  Vec2(0, -3)
+]
+
+// 9
+a) neither
+b) neither
+c) neither
+d) neither
