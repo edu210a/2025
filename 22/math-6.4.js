a47067220e9b24d51b72047998779bc4fb258c68
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-05-22 09:52:11 -0600

    Create math-6.4.js

---
+++ 22/math-6.4.js
@@ -0,0 +1,24 @@
+// #4-9, 12, 13
+4a)
+  Slope = 4
+  Intercept = 7
+
+4b)
+  Slope = 1
+  Intercept = 12
+
+4c)
+  Slope = 1
+  Intercept = 7
+
+4d)
+  Slope = 11
+  Intercept = 0
+
+4e)
+  Slope = 1
+  Intercept = 0
+
+4f)
+  Slope = 0
+  Intercept = 3
