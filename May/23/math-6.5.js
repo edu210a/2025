aeba317d26a259b597bcfbb8ec2e49023d18769f
Aedan Maki <108144902+edu210a@users.noreply.github.com>
2025-05-23 10:03:11 -0600

    Update math-6.5.js

--- May/23/math-6.5.js
+++ May/23/math-6.5.js
@@ -1,1 +1,49 @@
+4)
+  a)
+    Coordinates = [1, 5] 
+    Slope = -4
 
+  b)
+    Coordinates = [8, 7]
+    Slope = 3
+
+  c)
+    Coordinates = [15, 11]
+    Slope = null
+
+  d)
+    Coordinates = [2, 5]
+    Slope = 5
+
+  e)
+    Coordinates = [-3, 6]
+    Slope = (4 / 7)
+
+  f)
+    Coordinates = [16, 21]
+    Slope = -(8 / 5)
+
+5)
+  a) "y - 2 = -5(x + 2)"
+  b) "y - 6 = 7(x + 8)"
+  c) "y - 7 = -(3 / 4)(x + 5)"
+  d) "y + 8 = (x - 3)"
+
+6)
+  a) [{x: -4, y: 1}]
+  b) [{x: 3, y: -4}]
+  c) [{x: 2, y: 3}]
+  d) [{x: }]
+
+7)
+  a) "The line will rise steeply due to the positive slope, starting at (4, 2)."
+  b) "The line will rise moderately, starting at (3, 4)."
+  c) "The line will rise at a 45-degree angle and crosses the y-axis at -2."
+  d) "Like the previous one, it will also rise at a 45-degree angle and intersect the y-axis at -2."
+
+9)
+  a)
+    i) y - 4 = -1.3333333333333333(x - -2)
+    ii) y - 1 = 0.4(x - -2)
+    iii) y - -1 = 0.3333333333333333(x - -1)
+    iv) y - 3 = -2.5(x - -1)
